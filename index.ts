3ce6f212b8af20a3db40029201ccc2032c63b3bd
sspinit88 <39233740+sspinit88@users.noreply.github.com>
2023-09-05 18:09:10 +0400

    1 file modified

--- index.ts
+++ index.ts
@@ -16,10 +16,30 @@ interface Woman extends Person {
   braSize: number;
 }
 
-function getSize(person: Man | Woman): number {
-  return 
+function isWomanGuard(person: Man | Woman): person is Woman {
+  return (person as Woman).braSize !== undefined || 'braSize' in person;
 }
 
-function isWoman(person: Man | Woman): person is Woman {
-  
+function getSize(person: Man | Woman): number {
+  if (isWomanGuard(person)) {
+    return person.braSize;
+  }
+
+  return person.pantsSize;
 }
+
+const w: Woman = {
+  name: 'Masha',
+  age: 17,
+  braSize: 2,
+};
+
+const m: Man = {
+  name: 'Alex',
+  age: 78,
+  pantsSize: 3,
+};
+
+console.log(isWomanGuard(w));
+console.log(isWomanGuard(m));
+console.log(getSize(w));
