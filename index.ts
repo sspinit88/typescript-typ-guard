5113eb904537cec3f2dc274fde39c7bef464a2dc
sspinit88 <39233740+sspinit88@users.noreply.github.com>
2023-09-05 18:04:29 +0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,25 @@
 // Import stylesheets
 import './style.css';
+
+/* Type Guard */
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface Man extends Person {
+  pantsSize: number;
+}
+
+interface Woman extends Person {
+  braSize: number;
+}
+
+function getSize(person: Man | Woman): number {
+  return 
+}
+
+function isWoman(person: Man | Woman): person is Woman {
+  
+}
